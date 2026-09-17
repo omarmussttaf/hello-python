cf7791ba1d540b4698be356bb0c6042790d9ab6e
Omar Mussttaf <omarjazat@gmail.com>
2026-09-17 14:38:24 +0300

    simple calculater

---
+++ calculator.py
@@ -0,0 +1,13 @@
+#x = int(input("what is x? "))
+#y = int(input("what is y? "))
+
+#x = input ("what is x? ")
+#y = input ("what is y? ")
+
+#z=int (x)+int (y)
+x= float(input("what is x?"))
+y= float(input("what is y?"))
+
+z= round (x/y, 2)
+
+print("the answer is", f"{z:.2f}" )
