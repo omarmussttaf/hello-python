74149b07fadac6471284ab622821680a665d6204
Omar Mussttaf <omarjazat@gmail.com>
2026-09-17 13:33:32 +0300

    Complete first Python exercise

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
-print("Hello, World!")
+#ask user for their
+name = input("what is your name? ")
+
+#remove whitespace from str and capitalize the user's name
+name = name.strip().title()
+
+#split user's name into first and last name
+first, last = name.split(" ")
+
+#say hello to user
+print(f"Hello, {first}")
